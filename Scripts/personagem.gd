1c8b96a8f24dad9865bdce73027d32e9167976dc
Munir Abbas <munir.youssef.abbas@gmail.com>
2025-05-17 15:46:33 -0300

    Create key input

--- Scripts/personagem.gd
+++ Scripts/personagem.gd
@@ -8,6 +8,9 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	
+	if Input.is_action_just_pressed('punch'):
+		print("SOCO!")
+	
 	if Input.is_action_just_pressed('ui_left'):
 		animated_sprite_2d.flip_h = true
 	if Input.is_action_just_pressed('ui_right'):
@@ -19,10 +22,10 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		#animated_sprite_2d.animation = "run"
+		animated_sprite_2d.animation = "run"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		#animated_sprite_2d.animation = "idle"
+		animated_sprite_2d.animation = "idle"
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
